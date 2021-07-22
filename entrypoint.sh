c3363288fc463d304fe24beeb05bdf8884a59458
David Colburn <xero73@gmail.com>
2021-07-22 15:34:49 -0700

    dump 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,9 @@ rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 # Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
 pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
+# Load audio sink
 pactl load-module module-null-sink sink_name="grab" sink_properties=device.description="monitorOUT"
 
 # Start xvfb
 export DISPLAY=:10
-Xvfb :10 -ac -screen 0 1024x768x16 & node home/node/src/record
+Xvfb :10 -ac -screen 0 1920x1080x16 & node home/node/src/record
--- src/record.js
+++ src/record.js
@@ -4,8 +4,9 @@ const { exec, spawn } = require("child_process");
 (async () => {
 	const browser = await puppeteer.launch({
 		headless: false,
-		defaultViewport: null,
-		args: ['--start-fullscreen', '--no-sandbox']
+		defaultViewport: {width: 1920, height: 1080},
+		ignoreDefaultArgs: ["--enable-automation"],
+		args: ['--kiosk', '--no-sandbox', '--window-size=1920,1080']
 	});
 	const page = await browser.newPage();
 	await page.goto('https://example.livekit.io/#/room?url=wss%3A%2F%2Fdemo2.livekit.io&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjkzMjAyMDQsImlzcyI6IkFQSU1teGlMOHJxdUt6dFpFb1pKVjlGYiIsImp0aSI6InJyMSIsIm5iZiI6MTYyNjcyODIwNCwidmlkZW8iOnsiY2FuU3Vic2NyaWJlIjp0cnVlLCJoaWRkZW4iOnRydWUsInJvb20iOiJMS0hRIiwicm9vbUpvaW4iOnRydWV9fQ.pFg1z89kc47g5YL1bmkycRLl1NQQkHVDUxwnFUWlBBQ&videoEnabled=1&audioEnabled=1&simulcast=0');
@@ -20,12 +21,11 @@ const { exec, spawn } = require("child_process");
 
 	console.log('Start recording');
 	const ffmpeg = spawn('ffmpeg',  [
-		'-video_size', '1024x768',
-		'-framerate', '25',
-		'-f', 'x11grab', '-i', ':10.0',
-		'-f', 'pulse', '-i', 'grab.monitor',
-		'-ac', '2',
-		'-t', '10',
+		'-video_size', '1920x1080', '-framerate', '25',
+		'-f', 'x11grab', '-thread_queue_size', '1024', '-i', ':10.0', 
+		// '-vcodec', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency',
+		'-f', 'pulse', '-thread_queue_size', '1024', '-i', 'grab.monitor', '-ac', '2',
+		'-t', '15',
 		'recording.mp4'])
 	ffmpeg.stdout.pipe(process.stdout);
     ffmpeg.stderr.pipe(process.stderr);
@@ -38,6 +38,6 @@ const { exec, spawn } = require("child_process");
 		console.log('Closing');
 		ffmpeg.stdin.end();
 		await browser.close();
-	}, 1000 * 10);
+	}, 1000 * 15);
 
   })();
