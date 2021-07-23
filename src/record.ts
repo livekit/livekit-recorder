ca3e65101a8497ce46ac4ed9cfd61128d828c800
David Colburn <xero73@gmail.com>
2021-07-23 14:02:27 -0700

    sync

--- src/record.ts
+++ src/record.ts
@@ -3,9 +3,13 @@ import { Browser, Page, launch } from 'puppeteer'
 import { spawn } from 'child_process'
 
 type Config = {
-	Url: string | undefined
-	WSUrl: string | undefined
-	Token: string | undefined
+	Template?: {
+		Type: string
+		WSUrl: string
+		ApiKey: string
+		ApiSecret: string
+	}
+	Url?: string
 	Input: {
 		Width: number
 		Height: number
@@ -24,9 +28,7 @@ type Config = {
 }
 
 const defaultConfig: Config = {
-	Url: process.env.LIVEKIT_URL || "https://example.livekit.io/#/room",
-	WSUrl: process.env.LIVEKIT_WS_URL || "wss%3A%2F%2Fdemo2.livekit.io",
-	Token: process.env.LIVEKIT_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjkzMjAyMDQsImlzcyI6IkFQSU1teGlMOHJxdUt6dFpFb1pKVjlGYiIsImp0aSI6InJyMSIsIm5iZiI6MTYyNjcyODIwNCwidmlkZW8iOnsiY2FuU3Vic2NyaWJlIjp0cnVlLCJoaWRkZW4iOnRydWUsInJvb20iOiJMS0hRIiwicm9vbUpvaW4iOnRydWV9fQ.pFg1z89kc47g5YL1bmkycRLl1NQQkHVDUxwnFUWlBBQ",
+	Url: process.env.LIVEKIT_URL || "https://example.livekit.io/#/room?url=wss%3A%2F%2Fdemo2.livekit.io&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjkzMjAyMDQsImlzcyI6IkFQSU1teGlMOHJxdUt6dFpFb1pKVjlGYiIsImp0aSI6InJyMSIsIm5iZiI6MTYyNjcyODIwNCwidmlkZW8iOnsiY2FuU3Vic2NyaWJlIjp0cnVlLCJoaWRkZW4iOnRydWUsInJvb20iOiJMS0hRIiwicm9vbUpvaW4iOnRydWV9fQ.pFg1z89kc47g5YL1bmkycRLl1NQQkHVDUxwnFUWlBBQ&videoEnabled=0&audioEnabled=1&simulcast=0&recorder=1",
 	Input: {
 		Width: 1920,
 		Height: 1080,
@@ -37,8 +39,8 @@ const defaultConfig: Config = {
 		Location: (process.env.LIVEKIT_OUTPUT || 'recording.mp4'),
 		AudioBitrate: '128k',
 		AudioFrequency: '44100',
-		VideoBitrate: '1872k',
-		VideoBuffer: '3744k'
+		VideoBitrate: '2976k',
+		VideoBuffer: '5952k'
 	}
 }
 
@@ -48,6 +50,10 @@ function loadConfig(): Config {
 		return {...defaultConfig, ...JSON.parse(confString)}
 	}
 	return defaultConfig
+}
+
+function buildRecorderToken(key: string, secret: string): string {
+	return key+secret // TODO
 }
 
 (async () => {
@@ -76,31 +82,40 @@ function loadConfig(): Config {
 
 	// load room
 	const page: Page = await browser.newPage()
-	const url = `${conf.Url}?url=${conf.WSUrl}&token=${conf.Token}`
-	await page.goto(url);
-	// mute audio and video for example.livekit.io
+	let url: string
+	if (conf.Template) {
+		const token = buildRecorderToken(conf.Template.ApiKey, conf.Template.ApiSecret)
+		url = `https://recorder.livekit.io/${conf.Template.Type}?url=${encodeURIComponent(conf.Template.WSUrl)}&token=${token}`
+	} else if (conf.Url) {
+		url = conf.Url
+	} else {
+		throw Error('url or template required')
+	}
+	await page.goto(url)
+
+	// For testing
 	const [muteAudio] = await page.$x("//button[contains(., 'Mute')]")
 	if (muteAudio) {
 		await muteAudio.click()
 	}
-	const [muteVideo] = await page.$x("//button[contains(., 'Stop Video')]")
-	if (muteVideo) {
-		await muteVideo.click()
-	}
 
 	// prepare ffmpeg output
 	let ffmpegOutputOpts = [
+		// audio
+		'-c:a', 'aac', '-b:a', conf.Output.AudioBitrate, '-ar', conf.Output.AudioFrequency,
+		'-ac', '2', '-af', 'aresample=async=1',
+		// video
 		'-c:v', 'libx264', '-preset', 'veryfast', '-tune', 'zerolatency',
-		'-b:v', conf.Output.VideoBitrate, '-maxrate', conf.Output.VideoBitrate, '-bufsize', conf.Output.VideoBuffer,
-		'-c:a', 'aac', '-b:a', conf.Output.AudioBitrate, '-ar', conf.Output.AudioFrequency, '-ac', '2',
+		'-b:v', conf.Output.VideoBitrate,
 	]
 	if (conf.Output.Width && conf.Output.Height) {
 		ffmpegOutputOpts = ffmpegOutputOpts.concat('-s', `${conf.Output.Width}x${conf.Output.Height}`)
 	}
 
-	let ffmpegOutput: string[] = []
+	let ffmpegOutput: string[]
 	let uploadFunc: () => void
 	if (conf.Output.Location.startsWith('rtmp')) {
+		ffmpegOutputOpts = ffmpegOutputOpts.concat(['-maxrate', conf.Output.VideoBitrate, '-bufsize', conf.Output.VideoBuffer])
 		ffmpegOutput = ['-f', 'flv', conf.Output.Location]
 	} else if (conf.Output.Location.startsWith('s3://')) {
 		const filename = 'recording.mp4'
@@ -115,17 +130,19 @@ function loadConfig(): Config {
 	// spawn ffmpeg
 	console.log('Start recording')
 	const ffmpeg = spawn('ffmpeg', [
-		// video (x11 grab)
+		'-fflags', 'nobuffer', // reduce delay
 		'-fflags', '+igndts', // generate dts
-		'-thread_queue_size', '64', // avoid thread message queue blocking
+
+		// video (x11 grab)
+		'-thread_queue_size', '1024', // avoid thread message queue blocking
 		'-probesize', '42M', // increase probe size for bitrate estimation
+		// consider probesize 32 analyzeduration 0 for lower latency
 		'-s', `${conf.Input.Width}x${conf.Input.Height}`,
 		'-r', `${conf.Input.Framerate}`,
 		'-f', 'x11grab', '-i', `${xvfb.display()}.0`,
 
 		// audio (pulse grab)
-		'-fflags', '+igndts', // generate dts
-		'-thread_queue_size', '64', // avoid thread message queue blocking
+		'-thread_queue_size', '1024', // avoid thread message queue blocking
 		'-ac', '2', // 2 channels
 		'-f', 'pulse', '-i', 'grab.monitor',
 
@@ -141,18 +158,12 @@ function loadConfig(): Config {
 		uploadFunc && uploadFunc()
 	});
 
-	// stop recording
-	setTimeout(async () => {
-		console.log('Closing')
-		ffmpeg.kill('SIGINT')
-		await browser.close()
-	}, 1000 * 10);
-
-	// page.on('console', async (msg) => {
-	// 	if (msg.text() === 'END_RECORDING') {
-	// 		console.log('End recording')
-	// 		ffmpeg.kill('SIGINT')
-	// 		await browser.close()
-	// 	}
-	// })
+	// wait for END_RECORDING
+	page.on('console', async (msg) => {
+		if (msg.text() === 'END_RECORDING') {
+			console.log('End recording')
+			ffmpeg.kill('SIGINT')
+			await browser.close()
+		}
+	})
 })();
