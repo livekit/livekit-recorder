cb8aaae6cc36e75a6ba1cd7c5831cdb0a85c5c22
David Colburn <xero73@gmail.com>
2021-07-26 23:50:04 -0700

    remove server dep

--- worker/pkg/service/worker_test.go
+++ worker/pkg/service/worker_test.go
@@ -5,8 +5,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/livekit/livekit-server/pkg/recording"
+	"github.com/go-redis/redis/v8"
+	"github.com/livekit/protocol/utils"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/livekit/livekit-recording/worker/pkg/config"
 	"github.com/livekit/livekit-recording/worker/pkg/logger"
@@ -16,6 +19,7 @@ import (
 func TestWorker(t *testing.T) {
 	logger.Init("debug")
 
+	ctx := context.Background()
 	conf := config.TestConfig()
 	rc, err := StartRedis(conf)
 	require.NoError(t, err)
@@ -26,34 +30,32 @@ func TestWorker(t *testing.T) {
 		require.NoError(t, err)
 	}()
 
-	svc := recording.NewRoomRecorder(rc)
-
 	t.Run("Submit", func(t *testing.T) {
-		submit(t, svc, worker)
+		submit(t, ctx, rc, worker)
 		// wait to finish
 		time.Sleep(time.Millisecond * 5100)
 		require.Equal(t, Available, worker.status)
 	})
 
 	t.Run("Reserved", func(t *testing.T) {
-		submit(t, svc, worker)
-		submitReserved(t, svc)
+		submit(t, ctx, rc, worker)
+		submitReserved(t, rc)
 		// wait to finish
 		time.Sleep(time.Millisecond * 5100)
 		require.Equal(t, Available, worker.status)
 	})
 
 	t.Run("Stop", func(t *testing.T) {
-		id := submit(t, svc, worker)
+		id := submit(t, ctx, rc, worker)
 		// server ends recording
-		require.NoError(t, svc.EndRecording(context.Background(), id))
+		require.NoError(t, rc.Publish(ctx, utils.EndRecordingChannel(id), nil).Err())
 		time.Sleep(time.Millisecond * 50)
 		// check that recording has ended early
 		require.Equal(t, Available, worker.status)
 	})
 
 	t.Run("Kill", func(t *testing.T) {
-		submit(t, svc, worker)
+		submit(t, ctx, rc, worker)
 		// worker is killed
 		worker.Stop()
 		time.Sleep(time.Millisecond * 50)
@@ -62,7 +64,7 @@ func TestWorker(t *testing.T) {
 	})
 }
 
-func submit(t *testing.T, svc *recording.RoomRecorder, worker *Worker) string {
+func submit(t *testing.T, ctx context.Context, rc *redis.Client, worker *Worker) string {
 	// send recording reservation
 	req := &livekit.RecordingReservation{
 		SubmittedAt: time.Now().UnixNano(),
@@ -82,14 +84,14 @@ func submit(t *testing.T, svc *recording.RoomRecorder, worker *Worker) string {
 	}
 
 	// server sends reservation
-	id, err := svc.ReserveRecorder(context.Background(), req)
+	id, err := reserveRecorder(context.Background(), rc, req)
 	require.NoError(t, err)
 
 	// check that worker is reserved
 	require.Equal(t, Reserved, worker.status)
 
 	// start recording
-	require.NoError(t, svc.StartRecording(context.Background(), id))
+	require.NoError(t, rc.Publish(ctx, utils.StartRecordingChannel(id), nil).Err())
 	time.Sleep(time.Millisecond * 50)
 
 	// check that worker is recording
@@ -98,7 +100,7 @@ func submit(t *testing.T, svc *recording.RoomRecorder, worker *Worker) string {
 	return id
 }
 
-func submitReserved(t *testing.T, svc *recording.RoomRecorder) {
+func submitReserved(t *testing.T, rc *redis.Client) {
 	// send recording reservation
 	req := &livekit.RecordingReservation{
 		SubmittedAt: time.Now().UnixNano(),
@@ -118,6 +120,29 @@ func submitReserved(t *testing.T, svc *recording.RoomRecorder) {
 	}
 
 	// server sends reservation
-	_, err := svc.ReserveRecorder(context.Background(), req)
+	_, err := reserveRecorder(context.Background(), rc, req)
 	require.Error(t, err)
+}
+
+func reserveRecorder(ctx context.Context, rc *redis.Client, req *livekit.RecordingReservation) (string, error) {
+	id := utils.NewGuid(utils.RecordingPrefix)
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return "", err
+	}
+
+	sub := rc.Subscribe(ctx, utils.ReservationResponseChannel(id))
+	defer sub.Close()
+
+	err = rc.Publish(ctx, utils.ReservationChannel, string(b)).Err()
+	if err != nil {
+		return "", err
+	}
+
+	select {
+	case <-sub.Channel():
+		return id, nil
+	case <-time.After(utils.RecorderTimeout):
+		return "", errors.New("no recorders available")
+	}
 }
