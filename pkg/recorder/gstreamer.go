4f1bdcb9ab98ff1b4cd4d25855d4fa4b68288e2e
David Colburn <xero73@gmail.com>
2021-09-27 23:43:13 -0700

    first success

--- pkg/recorder/gstreamer.go
+++ pkg/recorder/gstreamer.go
@@ -7,72 +7,15 @@ import (
 	"github.com/livekit/protocol/logger"
 	"github.com/tinyzimmer/go-glib/glib"
 	"github.com/tinyzimmer/go-gst/gst"
+
+	"github.com/livekit/livekit-recorder/pkg/recorder/pipelines"
 )
 
 func LaunchGStreamer() error {
 	logger.Debugw("launching gstreamer")
 
-	for {
-		time.Sleep(time.Minute)
-		if false {
-			break
-		}
-	}
-
 	gst.Init(nil)
-
-	// audio in
-	audioIn, err := gst.NewElementWithName("pulsesrc", "audiosrc")
-	if err != nil {
-		return err
-	}
-
-	// video in
-	videoIn, err := gst.NewElementWithName("ximagesrc", "videosrc")
-	if err != nil {
-		return err
-	}
-	err = videoIn.SetProperty("use-damage", false)
-	if err != nil {
-		return err
-	}
-
-	// mux
-	mux, err := gst.NewElementWithName("mp4mux", "mux")
-	if err != nil {
-		return err
-	}
-
-	// output
-	output, err := gst.NewElementWithName("filesink", "videosink")
-	if err != nil {
-		return err
-	}
-	err = output.SetProperty("location", "test.mp4")
-	if err != nil {
-		return err
-	}
-
-	// build pipeline
-	pipeline, err := gst.NewPipeline("pipeline")
-	if err != nil {
-		return err
-	}
-	err = pipeline.AddMany(audioIn, videoIn, mux, output)
-	if err != nil {
-		return err
-	}
-
-	// link elements
-	err = gst.ElementLinkMany(audioIn, mux)
-	if err != nil {
-		return err
-	}
-	err = gst.ElementLinkMany(videoIn, mux)
-	if err != nil {
-		return err
-	}
-	err = gst.ElementLinkMany(mux, output)
+	pipeline, err := pipelines.OGG()
 	if err != nil {
 		return err
 	}
@@ -103,11 +46,13 @@ func LaunchGStreamer() error {
 		return err
 	}
 
+	go func() {
+		time.Sleep(time.Second * 15)
+		loop.Quit()
+	}()
+
 	// Block and iterate on the main loop
 	loop.Run()
-
-	time.Sleep(time.Second * 10)
-	loop.Quit()
 
 	return nil
 }
